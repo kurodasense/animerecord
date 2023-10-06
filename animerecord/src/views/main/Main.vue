bb740694d3ae4f1c1fbbeca05c9b12a704250f5f
kurodasense <12059181114@qq.com>
2023-10-06 12:02:35 +0800

    initial create

---
+++ animerecord/src/views/main/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="main">
+    <!-- 左侧导航栏 -->
+    <NavContent />
+    <!-- 右侧主要内容展示 -->
+    <CardItem />
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+  import NavContent from '../../components/Leftnav/NavContent.vue';
+  import CardItem from '../../components/Rightcontent/CardItem.vue';
+  export default {
+      components: { NavContent, CardItem }
+  }
+</script>
+<style scoped>
+  
+</style>
