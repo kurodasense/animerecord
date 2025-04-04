b1668fb8cd4bd5bb7071df74cd045c4f00c65909
kurodasense <1205918114@qq.com>
2025-04-04 22:28:36 +0800

    fix: 修改config文件配置

--- animerecord_nodejs/config/index.js
+++ animerecord_nodejs/config/index.js
@@ -1,6 +1,6 @@
 const SECRET = "kurodasense=114514";
 const USERNAME = "kurodasense-self";
-const KEYAPI = "AIzaSyCKDQpQzGPNY45n15nhkOwSxJ0DXAX12MY";
-const SEARCH_ENGINE_ID = "741fd23e809064c6b";
+const KEYAPI = "custom search api key"; // 替换为你的 API 密钥
+const SEARCH_ENGINE_ID = "search engine id"; // 替换为你的搜索引擎 ID
 
 module.exports = { SECRET, USERNAME, KEYAPI, SEARCH_ENGINE_ID };
